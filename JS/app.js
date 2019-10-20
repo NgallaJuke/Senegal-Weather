242a101ad159069ff4875addb069bee7bc2fe049
NgallaJuke <ngalladiouck@gmail.com>
2019-10-20 00:09:34 +0000

    adding localStorage

--- JS/app.js
+++ JS/app.js
@@ -52,4 +52,14 @@ cityForm.addEventListener('submit', e => {
         })
         .catch(error => { console.log(error); });
 
+    //set local storage
+
+    localStorage.setItem('city', city);
+
 });
+
+if (localStorage.getItem('city')) {
+    updateCity(localStorage.getItem('city'))
+        .then(data => updateUi(data))
+        .catch(error => console.log(error));
+}
